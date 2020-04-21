e7b2b6a82e16353c374fbb240949acaf4e2305c0
jin <aia.jin.202003@gmail.com>
2020-04-21 18:23:19 +0900

    제어자

---
+++ java_project/Cp9-Modifier/src/ex/Member.java
@@ -0,0 +1,27 @@
+package ex;
+
+public class Member {
+
+	private String name;
+	private String id;
+	
+	// getter / setter 메서드
+		
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+
+}
---
+++ java_project/Cp9-Modifier/src/ex/Singleton.java
@@ -0,0 +1,24 @@
+package ex;
+
+public class Singleton {
+	
+	// 1. 생성자의 접근제어지시자 : private -> 인스턴스 생성 불가
+	private Singleton() {}
+	
+	// 2. 공유할 객체를 생성
+	private static Singleton s = new Singleton();
+	
+	// 3. 참조값을 반환하는 메서들 생성
+	public static Singleton getInstance() {
+		return s;
+	}
+	
+	public void print() {
+		System.out.println("Singleton !!");
+	}
+	
+	
+	
+	
+
+}
