ee73edabb0bc4eef40e66681361a2cc18b8c27c3
jin <aia.jin.202003@gmail.com>
2020-04-24 17:35:38 +0900

    Friend Manager

---
+++ java_project/Cp10-Inheritance/src/friend/FriendMain.java
@@ -0,0 +1,43 @@
+package friend;
+
+public class FriendMain {
+
+	public static void main(String[] args) {
+		
+		FriendInfoHandler handler = new FriendInfoHandler(10);
+		
+		while(true) {
+			System.out.println("=== 메뉴를 선택해 주세요 =========");
+			System.out.println(" 1. 고교 친구 저장");
+			System.out.println(" 2. 대학 친구 저장");
+			System.out.println(" 3. 기본 정보 출력");
+			System.out.println(" 4. 전체 정보 출력");
+			System.out.println(" 5. 종료");
+			System.out.println("=============================");
+			
+			
+			int choice = handler.kb.nextInt();
+			// 1
+			handler.kb.nextLine(); // 앞의 버퍼 클리어
+			
+			switch(choice) {
+			case 1: case 2:
+				handler.addFriend(choice);
+				break;
+			case 3:
+				handler.showAllSimpleData();
+				break;
+			case 4:
+				handler.showAllData();
+				break;
+			case 5:
+				System.out.println("프로그램을 종료합니다.");
+				return;
+			}
+			
+			
+		}
+
+	}
+
+}
---
+++ java_project/Cp10-Inheritance/src/friend/HeighFriend.java
@@ -0,0 +1,25 @@
+package friend;
+
+public class HeighFriend extends Friend {
+
+	String work;	// 친구의 직업
+	
+	public HeighFriend(String name, String phoneNum, String addr, String work) {
+		super(name, phoneNum, addr);
+		this.work=work;
+		
+	}
+
+	@Override
+	void showData() {
+		showBasicInfo();
+		System.out.println("직업 : " + work);
+	}
+	
+	
+	
+	
+
+	
+	
+}
