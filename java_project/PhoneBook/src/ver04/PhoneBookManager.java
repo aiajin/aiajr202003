2a64faff43c15d0111dee2af00e0b4e37e56b744
jin <aia.jin.202003@gmail.com>
2020-04-27 13:19:40 +0900

    PhoneBook ver04

--- java_project/PhoneBook/src/ver04/PhoneBookManager.java
+++ java_project/PhoneBook/src/ver04/PhoneBookManager.java
@@ -144,16 +144,119 @@ public class PhoneBookManager {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
 	// 4. 배열의 정보 검색 : 이름 기준
+	void showInfo() {
+		
+		System.out.println("검색하실 이름을 입력해주세요.");
+		String name = kb.nextLine();
+		
+		int index = searchIndex(name);
+		
+		if(index<0) {
+			System.out.println("검색하신 이름의 정보가 없습니다.");
+		} else {
+			System.out.println("----------------");
+			books[index].showBasicInfo();
+			System.out.println("----------------");
+		}
+		
+		
+	}
+	
+	
+	
 	// 5. 배열의 정보를 삭제 : 이름 기준
+	void deleteInfo() {
+		System.out.println("삭제하고자하는 이름을 입력해주세요.");
+		String name = kb.nextLine();
+		
+		int index = searchIndex(name);
+		
+		if(index<0) {
+			System.out.println("삭제하고자하는 이름의 정보가 없습니다.");
+		} else {
+			
+			// 삭제 위치에서 외쪽으로 시프트
+			for(int i=index; i<numOfInfo-1; i++) {
+				books[i]=books[i+1];
+			}
+			// 삭제가 되었으므로 요소의 개수도 -1 
+			numOfInfo--;
+		}
+		
+	}
+	
+	
 	// 6. 배열의 정보를 수정 : 이름 기준
+	void editInfo() {
+		
+		System.out.println("변경하고자 하는 이름을 입력해주세요.");
+		String name = kb.nextLine();
+		
+		int index = searchIndex(name);
+		
+		if(index<0) {
+			System.out.println("찾으시는 이름의 정보가 존재하지 않습니다.");
+			return;
+		} else {
+			
+			String editName = books[index].name;
+			
+			System.out.println("수정 데이터 입력을 시작합니다.");
+			System.out.println("이름은 " + editName + "입니다.");
+			System.out.println("전화번호를 입력해주세요.");
+			String phoneNumber = kb.nextLine();
+			System.out.println("주소를 입력해주세요.");
+			String addr = kb.nextLine();
+			System.out.println("이메일을 입력해주세요.");
+			String email = kb.nextLine();
+			
+			PhoneInfo info = null;
+			
+			// 저장된 인스턴스가 : 기본, 대학, 회사, 동호회
+			if(books[index] instanceof PhoneInfo) {
+				info = new PhoneInfo(editName, phoneNumber, addr, email);
+				
+			} else if(books[index] instanceof PhoneUnivInfo) {
+				System.out.println("전공을 입력해주세요.");
+				String major = kb.nextLine();
+				System.out.println("학년을 입력해주세요.");
+				String grade = kb.nextLine();
+				
+				info = new PhoneUnivInfo(editName, phoneNumber, addr, email, major, grade);
+				
+			} else if(books[index] instanceof PhoneCompanyInfo) {
+				
+				System.out.println("회사 이름을 입력해주세요.");
+				String company = kb.nextLine();
+				System.out.println("부서 이름을 입력해주세요.");
+				String dept = kb.nextLine();
+				System.out.println("직급을 입력해주세요.");
+				String job = kb.nextLine();
+				
+				info = new PhoneCompanyInfo(editName, phoneNumber, addr, email, company, dept, job);
+				
+			
+			} else if(books[index] instanceof PhoneCafeInfo) {
+				
+				System.out.println("동호회 이름을 입력해주세요.");
+				String cafeName = kb.nextLine();
+				System.out.println("닉네임을 입력해주세요.");
+				String nickName = kb.nextLine();
+				
+				info = new PhoneCafeInfo(editName, phoneNumber, addr, email, cafeName, nickName);
+				
+			}
+			
+			// 배열에 새로운 인스턴스를 저장
+			books[index]=info;
+			
+			
+		}
+		
+		
+		
+	}
 	
 	
 	
