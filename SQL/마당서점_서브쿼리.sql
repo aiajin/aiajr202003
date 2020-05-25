ad1ea81030478ac140d27b43659bb1c842599259
jin <aia.jin.202003@gmail.com>
2020-05-25 13:18:12 +0900

    Update 마당서점_서브쿼리.sql

--- SQL/마당서점_서브쿼리.sql
+++ SQL/마당서점_서브쿼리.sql
@@ -1,74 +1,220 @@
--- practice 06 마당서점
+-- 마당서점 sub query
 select * from book;
 select * from customer;
 select * from orders;
 
 --1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
---(5) 박지성이구매한도서의출판사수
-select count(publisher) from book 
-where bookid in (select bookid from orders o join customer c using(custid) where name='박지성');
 
---(6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
-select bookname, abs(price-saleprice) Pricegap 
-from orders o join book b 
-using(bookid) 
-where o.custid = (select custid from customer where name='박지성');
+--(5) 박지성이 구매한 
+--    도서의 출판사수
 
---(7) 박지성이구매하지않은도서의이름
-select bookname from book 
-where bookid not in (select bookid from orders o, customer c where o.custid=c.custid and c.name='박지성');
+SELECT COUNT(DISTINCT PUBLISHER)
+FROM CUSTOMER C, ORDERS O, BOOK B
+WHERE C.CUSTID=O.CUSTID AND O.BOOKID=B.BOOKID
+AND C.NAME='박지성'
+;
+
+-- 박지성이 구매한 도서 ID
+SELECT DISTINCT O.BOOKID
+FROM ORDERS O, CUSTOMER C
+WHERE O.CUSTID=C.CUSTID AND C.NAME='박지성'
+;
+
+SELECT COUNT(DISTINCT PUBLISHER)
+FROM BOOK
+WHERE BOOKID IN (
+    SELECT DISTINCT O.BOOKID
+    FROM ORDERS O, CUSTOMER C
+    WHERE O.CUSTID=C.CUSTID AND C.NAME='박지성'
+
+)
+;
+
+
+
+
+
+--(6) 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
+
+SELECT b.bookname, b.price, B.PRICE-o.saleprice AS PRICEGAP
+FROM ORDERS O, CUSTOMER C, BOOK B
+WHERE C.CUSTID=O.CUSTID AND O.BOOKID=B.BOOKID
+AND C.NAME='박지성'
+;
+
+SELECT b.bookname, B.PRICE, B.PRICE-o.saleprice
+FROM ORDERS O, BOOK B
+WHERE O.BOOKID=B.BOOKID
+AND CUSTID=(SELECT CUSTID FROM CUSTOMER WHERE NAME='박지성')
+;
+SELECT CUSTID FROM CUSTOMER WHERE NAME='박지성';
+
+
+
+
+
+
+
+
+--(7) 박지성이 구매하지 않은 
+--    도서의 이름
+
+SELECT *
+FROM ORDERS O, CUSTOMER C
+WHERE O.CUSTID=C.CUSTID AND C.NAME='박지성'
+;
+
+
+SELECT BOOKNAME, BOOKID
+FROM BOOK
+WHERE BOOKID NOT IN (
+    SELECT O.BOOKID
+    FROM ORDERS O, CUSTOMER C
+    WHERE O.CUSTID=C.CUSTID AND C.NAME='박지성'
+)
+;
+
+
+
 
 
 --2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
---(8) 주문하지않은고객의이름(부속질의사용)
-select name from customer where custid not in (select distinct custid from orders);
+--(8) 주문하지 않은 고객의 이름(부속질의사용)
 
---(9) 주문금액의총액과주문의평균금액
-select sum(saleprice) Total, avg(saleprice) AvgSalePrice from orders;
+SELECT C.NAME
+FROM ORDERS O, CUSTOMER C
+WHERE O.CUSTID(+)=C.CUSTID 
+AND O.ORDERID IS NULL
+;
 
---(10) 고객의이름과고객별구매액
-select name, sum(saleprice) from orders o join customer c using(custid) group by name;
+SELECT NAME
+FROM CUSTOMER
+WHERE CUSTID NOT IN (SELECT DISTINCT CUSTID FROM ORDERS)
+;
 
---(11) 고객의이름과고객이구매한도서목록 
-select name, bookname 
-from (select * from orders o, customer c, book b where o.custid=c.custid and o.bookid = b.bookid) 
-order by name;
+SELECT DISTINCT CUSTID FROM ORDERS ;
 
---(12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
-select * from orders o join book b on o.bookid=b.bookid 
-where abs(price-saleprice)>= all(
-    select abs(price-saleprice) 
-    from (select * from orders o, customer c, book b where o.custid=c.custid and o.bookid = b.bookid));
 
---(13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
-select name 
-from customer c join orders o 
-on c.custid= o.custid 
-group by name
-having avg(saleprice) > (select avg(price) from book b, orders o where b.bookid=o.bookid);
+
+
+
+
+--(9) 주문 금액의 총액과 주문의 평균금액
+SELECT SUM(SALEPRICE), AVG(SALEPRICE)
+FROM ORDERS
+;
+
+
+
+
+
+
+
+
+
+
+--(10) 고객의 이름과 고객별 구매액
+SELECT C.NAME, SUM(SALEPRICE)
+FROM ORDERS O, CUSTOMER C
+WHERE O.CUSTID=C.CUSTID
+GROUP BY C.NAME
+;
+
+
+
+
+
+--(11) 고객의 이름과 고객이 구매한 도서 목록 
+
+SELECT C.NAME, B.BOOKNAME
+FROM BOOK B, ORDERS O, CUSTOMER C
+WHERE B.BOOKID=O.BOOKID AND O.CUSTID=C.CUSTID
+;
+
+
+
+
+
+
+
+--(12) 도서의 가격(Book 테이블)과 판매가격(Orders 테이블)의
+--     차이가 가장 많은 주문
+
+SELECT MAX(B.PRICE-O.SALEPRICE)
+FROM ORDERS O, BOOK B
+WHERE O.BOOKID=B.BOOKID
+;
+
+
+SELECT B.BOOKNAME, B.PRICE-O.SALEPRICE
+FROM ORDERS O, BOOK B
+WHERE O.BOOKID=B.BOOKID
+AND PRICE-SALEPRICE=(
+    SELECT MAX(B.PRICE-O.SALEPRICE)
+    FROM ORDERS O, BOOK B
+    WHERE O.BOOKID=B.BOOKID
+)
+;
+
+
+
+
+
+
+--(13) 도서의 판매액 평균 보다 
+--     자신의 구매액평균이 더높은 고객의 이름
+
+SELECT AVG(SALEPRICE) FROM ORDERS;
+
+
+SELECT C.NAME, AVG(SALEPRICE)
+FROM ORDERS O, CUSTOMER C
+WHERE O.CUSTID=C.CUSTID
+GROUP BY C.NAME
+HAVING AVG(SALEPRICE)>(
+    SELECT AVG(SALEPRICE) FROM ORDERS
+)-- 평균 구매액
+;
+
+
+
+
   
 --3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
 --(1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-select name from orders o, customer c, book b where o.custid=c.custid and o.bookid = b.bookid
-and publisher in (
-    select publisher 
-    from orders o natural join book b 
-    where custid = (select custid from customer where name='박지성'))
-and name != '박지성';
+
+SELECT B.PUBLISHER
+FROM ORDERS O, CUSTOMER C, BOOK B
+WHERE O.CUSTID=C.CUSTID AND O.BOOKID=B.BOOKID
+AND C.NAME='박지성'
+;
+
+
+SELECT *
+FROM ORDERS O, CUSTOMER C, BOOK B
+WHERE O.CUSTID=C.CUSTID AND O.BOOKID=B.BOOKID
+
+AND B.PUBLISHER IN (
+    SELECT DISTINCT B.PUBLISHER
+    FROM ORDERS O, CUSTOMER C, BOOK B
+    WHERE O.CUSTID=C.CUSTID AND O.BOOKID=B.BOOKID
+    AND C.NAME='박지성'
+)
+
+AND C.NAME!='박지성'
+
+;
+
+
+
+
 
 --(2)***** 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
-select name from orders o, customer c, book b where o.custid=c.custid and o.bookid = b.bookid
-group by name
-having count(distinct publisher)>=2;
 
 
 
-select o.custid, sum(o.saleprice)
-from customer c, orders o
-where c.custid = o.custid
-group by o.custid
-order by o.custid
-;
+
+
 
 
 
