387a6e22b6643ba3c1b57c53085f8f8af86e4d20
jin <aia.jin.202003@gmail.com>
2020-05-25 14:46:04 +0900

    Update 마당서점_서브쿼리.sql

--- SQL/마당서점_서브쿼리.sql
+++ SQL/마당서점_서브쿼리.sql
@@ -209,7 +209,14 @@ AND C.NAME!='박지성'
 
 
 
---(2)***** 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+--(2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+
+SELECT C.NAME, COUNT(DISTINCT PUBLISHER)
+FROM ORDERS O, CUSTOMER C, BOOK B
+WHERE O.CUSTID=C.CUSTID AND O.BOOKID=B.BOOKID
+GROUP BY C.NAME
+HAVING COUNT(DISTINCT PUBLISHER) >= 2
+;
 
 
 
