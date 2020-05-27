f2a2c96e56a1300edab4ca0edb947f0a0c8355b8
jin <aia.jin.202003@gmail.com>
2020-05-27 18:28:06 +0900

    Update 2020.05.27_view.sql

--- SQL/2020.05.27_view.sql
+++ SQL/2020.05.27_view.sql
@@ -39,6 +39,27 @@ select rownum, empno, ename, hiredate from emp order by hiredate;
 
 
 
+----------------------------------------------------------------
+-- sequence : 번호 자동 생성기
+----------------------------------------------------------------
+
+-- create sequence 시퀀스이름 
+-- start with 시작값  0 시작
+-- increment by 증가값 1
+-- maxvalue 최대값
+-- minvalue 최소값
+-- cycle | nocycle 반복 여부
+-- cache | nocache
+
+-- 부서번호 deptno 의 sequence 생성
+create sequence dept_deptno_seq01;
+
+select dept_deptno_seq01.nextval
+from dual;
+
+select dept_deptno_seq01.currval
+from dual;
+
 
 
 
