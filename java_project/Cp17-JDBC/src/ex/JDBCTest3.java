f20e6239de1de4fc77ee1cdb2213076c18b0cf91
jin <aia.jin.202003@gmail.com>
2020-05-29 18:07:34 +0900

    JDBC

--- java_project/Cp17-JDBC/src/ex/JDBCTest3.java
+++ java_project/Cp17-JDBC/src/ex/JDBCTest3.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JDBCTest3 {
 
@@ -30,18 +31,45 @@ public class JDBCTest3 {
 			//3. Statement : Connection
 			Statement stmt = conn.createStatement();
 			
-			String sql = "select * from dept order by deptno";			
+			
+			Scanner kb = new Scanner(System.in);
+			
+			System.out.println("회원의 이름을 입력해주세요. >> ");
+			String searchName = kb.nextLine();
+			
+			
+			
+			String sql = "select empno, ename, job from emp order by ename";
+			String sql1 = "select o.orderid, c.name, b.bookname  from customer c, book b, orders o "
+					    + " where c.custid=o.custid and o.bookid=b.bookid  "
+					    + " and c.name='"+searchName+"'"; 
+			
+			System.out.println("sql : "+ sql1);
+			System.out.println();
+			System.out.println();
+			System.out.println();
 			
 			// Select 의 결과는 ResultSet 이 받는다.
 			// executeQuery(sql문) -> ResultSet
-			rs = stmt.executeQuery(sql);
+			rs = stmt.executeQuery(sql1);
 			
 			// ResultSet : next() -> 행의 존재 유무 확인
 			
+			//System.out.println("사원 리스트 ");
+			//System.out.println("--------------------------");
+			//System.out.println("사원번호\t사원이름\t 직급");
+			//System.out.println("--------------------------");
+			
+			System.out.println("판매 리스트");
+			System.out.println("---------------------------------");
+			System.out.println("판매 아이디\t회원이름\t책이름");
+			System.out.println("---------------------------------");
+			
+			
 			while(rs.next()) {
-//				System.out.print(rs.getInt("deptno") + "\t");
-//				System.out.print(rs.getString("dname") + "\t");
-//				System.out.print(rs.getString("loc") + "\n");
+				//System.out.print(rs.getInt("empno") + "\t");
+				//System.out.print(rs.getString("ename") + "\t");
+				//System.out.print(rs.getString("job") + "\n");
 				System.out.print(rs.getInt(1) + "\t");
 				System.out.print(rs.getString(2) + "\t");
 				System.out.print(rs.getString(3) + "\n");
