9dfdea1dc7912cdf60d34e0f1f03837f8d6341eb
jin <aia.jin.202003@gmail.com>
2020-06-01 14:53:29 +0900

    emp 수정 기능 추가

--- java_project/EMP-Manager/src/manager/EMPManager.java
+++ java_project/EMP-Manager/src/manager/EMPManager.java
@@ -14,20 +14,23 @@ public class EMPManager {
 
 	public static void main(String[] args) {
 
-		System.out.println("Select Menu");
-		System.out.println("----------------");
-		System.out.println("1. EMP,  2. DEPT");
-		System.out.println("----------------");
+		while (true) {
+			System.out.println("Select Menu");
+			System.out.println("----------------");
+			System.out.println("1. EMP,  2. DEPT");
+			System.out.println("----------------");
 
-		int select = sc.nextInt();
+			int select = sc.nextInt();
 
-		switch (select) {
-		case 1:
-			empManager();
-			break;
-		case 2:
-			deptManager();
-			break;
+			switch (select) {
+			case 1:
+				empManager();
+				break;
+			case 2:
+				deptManager();
+				break;
+			}
+
 		}
 
 	}
@@ -73,15 +76,12 @@ public class EMPManager {
 		Statement stmt = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		// 사용자 입력정보 변수
-		
+
 		System.out.println("수정하고자 하는 사원이름 : ");
 		sc.nextLine();
 		String searchName = sc.nextLine();
-
-		
-		
 
 		try {
 			// 0. 드라이버 LIB 추가
@@ -105,75 +105,67 @@ public class EMPManager {
 			// Statement or PreparedStatement
 			// pstmt = conn.prepareStatement(SQL 문장)
 
-			
 			// 주의 !!!!!
 			// 입력된 수정하고자 하는 이름의 데이터가 존재해야 수정 데이터 입력이 시작시킵니다.
 			// 그리고 이름의 데이터는 유일조건이 있어야 합니다.
 			// 유일조건이 아니라면 여러개의 행에 수정 처리가 이루어집니다.
 			// 현재 버전에서는 유일한 값으로 생각하고 처리합니다.
-			
-			stmt = conn.createStatement();
-			
-			String selectSql = "select from emp where ename='"+searchName+"'";
-			
-			
-			
-			
-			
-			
-			
 
-			
+			stmt = conn.createStatement();
+
+			String selectSql = "select * from emp where ename='" + searchName + "'";
+
+			rs = stmt.executeQuery(selectSql);
+
+			int sEmpno = 0;
+			String sEname = "";
+			int sDeptno = 0;
+			int sSal = 0;
+
+			if (rs.next()) {
+				sEmpno = rs.getInt("empno");
+				sEname = rs.getString("ename");
+				sSal = rs.getInt("sal");
+				sDeptno = rs.getInt("deptno");
+			} else {
+				System.out.println("검색하신 이름의 데이터가 존재하지 않습니다.");
+				return;
+			}
+
 			// 사용자 입력정보 변수
 			System.out.println("사원 정보를 입력해주세요.");
 
-			System.out.println("사원번호 : ");
-			int empno = sc.nextInt();
-			System.out.println("사원이름 : ");
-			sc.nextLine();
+			System.out.println("사원번호 : " + sEmpno);
+			System.out.println("사원번호는 수정되지 않습니다.");
+
+			System.out.println("사원이름 ( " + sEname + "  ) : ");
 			String ename = sc.nextLine();
-			System.out.println("직급(업무) : ");
-			String job = sc.nextLine();
-			System.out.println("관리자 : ");
-			int mgr = sc.nextInt();
-			System.out.println("입사일 : ");
-			sc.nextLine();
-			String hiredate = sc.nextLine();
-			System.out.println("급여 : ");
+
+			System.out.println("급여( " + sSal + "  ) : ");
 			int sal = sc.nextInt();
-			System.out.println("수당 : ");
-			int comm = sc.nextInt();
-			System.out.println("부서번호 : ");
+
+			System.out.println("부서번호( " + sDeptno + "  ) : ");
 			int deptno = sc.nextInt();
 
 			// 공백 입력에 대한 예외처리가 있어야 하나 이번 버전에서는 모두 잘 입력된것으로 처리합니다.
-			
-			
-			
-			
-			
-			
-			
-			String sql = "insert into emp " + " (empno, ename, job, mgr, hiredate, sal, comm, deptno) "
-					+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+
+			String sql = "update emp  set  " + " ename=?, sal=?, deptno=? " + " where empno=?";
 
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, empno);
-			pstmt.setString(2, ename);
-			pstmt.setString(3, job);
-			pstmt.setInt(4, mgr);
-			pstmt.setString(5, hiredate);
-			pstmt.setInt(6, sal);
-			pstmt.setInt(7, comm);
-			pstmt.setInt(8, deptno);
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, ename);
+			pstmt.setInt(2, sal);
+			pstmt.setInt(3, deptno);
+			pstmt.setInt(4, sEmpno);
 
 			int resultCnt = pstmt.executeUpdate();
 
 			if (resultCnt > 0) {
-				System.out.println("정상적으로 입력 되었습니다.");
-				System.out.println(resultCnt + "행이 입력되었습니다.");
+				System.out.println("정상적으로 수정 되었습니다.");
+				System.out.println(resultCnt + "행이 수정되었습니다.");
 			} else {
-				System.out.println("입력이 되지않았습니다. 확인후 재 시도해주세요.");
+				System.out.println("수정이 되지않았습니다. 확인후 재 시도해주세요.");
 			}
 
 			// 4. 데이터베이스 연결 종료
@@ -190,6 +182,15 @@ public class EMPManager {
 			if (rs != null) {
 				try {
 					rs.close();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -215,7 +216,7 @@ public class EMPManager {
 			}
 
 		}
-		
+
 	}
 
 	private static void empList() {
@@ -247,33 +248,31 @@ public class EMPManager {
 			conn = DriverManager.getConnection(url, user, pw);
 
 			String sql = "select * from emp  order by ename";
-			
-			stmt = conn.createStatement();
-			
-			rs = stmt.executeQuery(sql);
 
+			stmt = conn.createStatement();
+
+			rs = stmt.executeQuery(sql);
 
 			int resultCnt = 0;
 			System.out.println("검색 결과");
 			System.out.println("=======================================================================");
-			while(rs.next()) {
-				System.out.print(rs.getInt("empno")+"\t");
-				System.out.printf("%6s", rs.getString("ename")+"\t");
-				System.out.printf("%9s", rs.getString("job")+"\t");
-				System.out.print(rs.getInt("mgr")+"\t");
-				System.out.print(rs.getString("hiredate").substring(0, 10)+"\t");
-				System.out.print(rs.getInt("sal")+"\t");
-				System.out.print(rs.getInt("comm")+"\t");
-				System.out.print(rs.getInt("deptno")+"\n");
+			while (rs.next()) {
+				System.out.print(rs.getInt("empno") + "\t");
+				System.out.printf("%6s", rs.getString("ename") + "\t");
+				System.out.printf("%9s", rs.getString("job") + "\t");
+				System.out.print(rs.getInt("mgr") + "\t");
+				System.out.print(rs.getString("hiredate").substring(0, 10) + "\t");
+				System.out.print(rs.getInt("sal") + "\t");
+				System.out.print(rs.getInt("comm") + "\t");
+				System.out.print(rs.getInt("deptno") + "\n");
 				resultCnt++;
 			}
-			if(resultCnt<1) {
+			if (resultCnt < 1) {
 				System.out.println("검색 결과가 없습니다.");
 			}
-			
+
 			System.out.println("=======================================================================");
-			
-			
+
 			// 4. 데이터베이스 연결 종료
 			// pstmt.close();
 			// conn.close();
@@ -314,7 +313,6 @@ public class EMPManager {
 
 		}
 
-		
 	}
 
 	private static void empDelete() {
@@ -326,11 +324,10 @@ public class EMPManager {
 		ResultSet rs = null;
 
 		// 사용자 입력정보 변수
-		
+
 		System.out.println("삭제하고자 하는 사원이름 : ");
 		sc.nextLine();
 		String searchName = sc.nextLine();
-
 
 		// 공백 입력에 대한 예외처리가 있어야 하나 이번 버전에서는 모두 잘 입력된것으로 처리합니다.
 
@@ -358,15 +355,14 @@ public class EMPManager {
 			pstmt.setString(1, searchName);
 			int resultCnt = pstmt.executeUpdate();
 
-			if(resultCnt<1) {
+			if (resultCnt < 1) {
 				System.out.println("삭제할 정보가 검색 결과가 없습니다.");
 			} else {
 				System.out.println(resultCnt + "행이 삭제 되었습니다.");
 			}
-			
+
 			System.out.println("=================================");
-			
-			
+
 			// 4. 데이터베이스 연결 종료
 			// pstmt.close();
 			// conn.close();
@@ -406,7 +402,7 @@ public class EMPManager {
 			}
 
 		}
-		
+
 	}
 
 	private static void empSearch() {
@@ -417,11 +413,10 @@ public class EMPManager {
 		ResultSet rs = null;
 
 		// 사용자 입력정보 변수
-		
+
 		System.out.println("검색하고자 하는 사원이름 : ");
 		sc.nextLine();
 		String searchName = sc.nextLine();
-
 
 		// 공백 입력에 대한 예외처리가 있어야 하나 이번 버전에서는 모두 잘 입력된것으로 처리합니다.
 
@@ -446,18 +441,16 @@ public class EMPManager {
 			// 3. SQL 처리
 			// Statement or PreparedStatement
 			// pstmt = conn.prepareStatement(SQL 문장)
-			
-			
+
 			// Mysql
 			// "SELECT * FROM epm WHERE name LIKE ?"
-			// psmt.setString(1, "%"+name+"%"); 
+			// psmt.setString(1, "%"+name+"%");
 
-			//			Oracle
-			// select * from emp  where ename like '%'||?||'%'
-			
+			// Oracle
+			// select * from emp where ename like '%'||?||'%'
 
 			String sql = "select * from emp  where ename like '%'||?||'%'";
-			//String sql = "select * from emp  where ename=?";
+			// String sql = "select * from emp where ename=?";
 
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, searchName);
@@ -466,24 +459,23 @@ public class EMPManager {
 			int resultCnt = 0;
 			System.out.println("검색 결과");
 			System.out.println("=======================================================================");
-			while(rs.next()) {
-				System.out.print(rs.getInt("empno")+"\t");
-				System.out.print(rs.getString("ename")+"\t");
-				System.out.print(rs.getString("job")+"\t");
-				System.out.print(rs.getInt("mgr")+"\t");
-				System.out.print(rs.getString("hiredate").substring(0, 10)+"\t");
-				System.out.print(rs.getInt("sal")+"\t");
-				System.out.print(rs.getInt("comm")+"\t");
-				System.out.print(rs.getInt("deptno")+"\n");
+			while (rs.next()) {
+				System.out.print(rs.getInt("empno") + "\t");
+				System.out.print(rs.getString("ename") + "\t");
+				System.out.print(rs.getString("job") + "\t");
+				System.out.print(rs.getInt("mgr") + "\t");
+				System.out.print(rs.getString("hiredate").substring(0, 10) + "\t");
+				System.out.print(rs.getInt("sal") + "\t");
+				System.out.print(rs.getInt("comm") + "\t");
+				System.out.print(rs.getInt("deptno") + "\n");
 				resultCnt++;
 			}
-			if(resultCnt<1) {
+			if (resultCnt < 1) {
 				System.out.println("검색 결과가 없습니다.");
 			}
-			
+
 			System.out.println("=======================================================================");
-			
-			
+
 			// 4. 데이터베이스 연결 종료
 			// pstmt.close();
 			// conn.close();
