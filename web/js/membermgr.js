50e251b59dfef725d85afdb635435ac5c4fc2899
jin <aia.jin.202003@gmail.com>
2020-06-16 16:16:17 +0900

    입력데이터에 따라 리스트 출력 - 테이블 이용

--- web/js/membermgr.js
+++ web/js/membermgr.js
@@ -27,27 +27,60 @@ Member.prototype.toString = function() {
 // 데이터 저장함수
 function addMember(member){
     members.push(member);
-
-    console.log(members)
+    console.log(members);
+    displayTable();
 }
 
 function createMember(){
-
-    alert(111);
+    // 사용자 입력 데이터 받기, document로 케스팅 후 value 속성 사용
     var id=document.getElementById('uid').value;
     var name=document.getElementById('uname').value;
     var pw=document.getElementById('pw').value;
+    // 배열에 데이터 추가
+    addMember(new Member(id,name,pw)); 
 
-    members.push(new Member(id,name,pw)); 
-    
-    console.log(members);
-
+    // form 테그의 action 실행이 되지 않도록 반드시 return false
     return false;
     
+}
+
+function displayTable(){
+
+    var listHtml = '';
+    listHtml += '<table  class="listtable">';
+    listHtml += '   <tr>';
+    listHtml += '       <th>index</th>';
+    listHtml += '       <th>ID(이메일)</th>';
+    listHtml += '       <th>비밀번호</th>';
+    listHtml += '       <th>이름</th>';
+    listHtml += '       <th>관리</th>';
+    listHtml += '   </tr>';
+
+    // 배열의 요소를 반복문으로 테그 생성
+    for(let i=0; i<members.length; i++){
+        listHtml += '   <tr>';
+        listHtml += '       <td>'+i+'</td>';
+        listHtml += '       <td>'+members[i].id+'</td>';
+        listHtml += '       <td>'+members[i].pw+'</td>';
+        listHtml += '       <td>'+members[i].name+'</td>';
+        listHtml += '       <td> 수정 | 삭제 </td>';
+        listHtml += '   </tr>';
+    }
+
+    listHtml += '</table>';
+
+    var listTable = document.getElementById('list');
+    listTable.innerHTML=listHtml;
+
 }
 
 
 // 저장, 수정 submit 이벤트 처리
 window.onload = function(){
+
+    displayTable();
+
+
+    // 입력 폼 casting -> onsubmit Event 
     document.getElementById('regForm').onsubmit = createMember;
 }
