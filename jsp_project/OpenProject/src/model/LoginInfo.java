05651a5ff931b01b352ed025405cad103cbc3a85
jin <aia.jin.202003@gmail.com>
2020-07-03 15:06:13 +0900

    로그인 정보 저장 객체

---
+++ jsp_project/OpenProject/src/model/LoginInfo.java
@@ -0,0 +1,56 @@
+package model;
+
+public class LoginInfo {
+
+	private String uid;
+	private String uname;
+	private String email;
+	private String photo;
+
+	public LoginInfo(String uid, String uname, String email, String photo) {
+		this.uid = uid;
+		this.uname = uname;
+		this.email = email;
+		this.photo = photo;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginInfo [uid=" + uid + ", uname=" + uname + ", email=" + email + ", photo=" + photo + "]";
+	}
+
+	
+
+}
