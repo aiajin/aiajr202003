25cd2b4efdc77658e76e424ae054f3ec65295460
jin <aia.jin.202003@gmail.com>
2020-07-07 16:13:23 +0900

    Create Emp.java

---
+++ jsp_project/FirstWeb/src/model/Emp.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Emp {
+
+	private int empno;
+	private String ename;
+	private int sal;
+	private String job;
+
+	public Emp(int empno, String ename, int sal, String job) {
+		this.empno = empno;
+		this.ename = ename;
+		this.sal = sal;
+		this.job = job;
+	}
+
+	public Emp() {
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", sal=" + sal + ", job=" + job + "]";
+	}
+	
+	
+	
+	
+
+}
