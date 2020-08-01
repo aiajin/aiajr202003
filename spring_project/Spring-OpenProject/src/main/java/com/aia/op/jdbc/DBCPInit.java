97ce83e5626e26787ce47fd42ec6c48aa53533a1
유영진 <contentsware@gmail.com>
2020-08-01 11:14:25 +0900

    Db Driver, Pool 등록 Servlet

--- spring_project/Spring-OpenProject/src/main/java/com/aia/op/jdbc/DBCPInit.java
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/jdbc/DBCPInit.java
@@ -27,7 +27,7 @@ public class DBCPInit extends HttpServlet {
 		try {
 			// 커넥션 풀이 내부에서 사용할 jdbc 드라이버를 로딩함.
 			//Class.forName("com.mysql.jdbc.Driver");
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			//Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("Mysql 데이터베이스 드라이버 로드 성공...!!!!");
 		} catch (ClassNotFoundException ex) {
