64d7653ed204050b10e048a6f4c1174ac900031f
ctwarebook <contentsware@gmail.com>
2020-08-10 11:51:53 +0900

    Exception 처리

--- spring_project/Spring-OpenProject/src/main/java/com/aia/op/IndexController.java
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/IndexController.java
@@ -1,6 +1,7 @@
 package com.aia.op;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,29 @@ public class IndexController {
 	
 	@RequestMapping("/index")
 	public String index() {
+		
+//		String str = null;
+//		str.toString();
+		
 		return "index";
 	}
 
+	@ExceptionHandler(NullPointerException.class)
+	public String handleNullPointerException(NullPointerException e) {
+		
+		e.printStackTrace();
+		
+		return "error/nullPointer";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ spring_project/Spring-OpenProject/src/main/webapp/WEB-INF/views/error/nullPointer.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>요청을 처리하는 과정에서 예외(NullPoinerException)가 발생했습니다.</h1>
+	<a href="${pageContext.request.contextPath}">홈으로 가기</a>
+</body>
+</html>
